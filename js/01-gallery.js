8998a51985a5658aaca869805490368242c855ca
MariOkner <99042644+MariOkner@users.noreply.github.com>
2022-08-12 22:30:09 +0200

    перша перевірка дз7

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// console.log(galleryItems);
+
+document.addEventListener('keydown', onKeyDown);
+const lightbox = basicLightbox.create('');
+
+const galleryEl = document.querySelector('.gallery');
+const galleryImages = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryImages);
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function createGallery() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__list">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>    
+        </li>
+        `; 
+    }).join('');  
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') return;
+    
+    lightbox.element().innerHTML = 
+    `<img class="active"
+        src="${event.target.dataset.source}" width="800" height="600"
+    />`;
+    lightbox.show();
+}
+
+function onKeyDown(event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+        lightbox.close();
+    }
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryImages = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryImages);
+
+galleryEl.addEventListener('click', onGalleryClick)
+
+function createGallery() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__list">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    title="${description}"
+                />
+            </a>
+        </li>
+        `; 
+    }).join('');  
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') return;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'tittle',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    className: 'lightbox',
+    enableKeyboard: true,   
+});
+    lightbox.next();
